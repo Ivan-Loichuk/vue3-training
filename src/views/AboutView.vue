403585d5389e56c11e28ddf819de877241ff4fe1
Ivan Loichuk <ivan.loichuk@gmail.com>
2023-02-20 12:26:56 +0100

    fix: about page placeholder

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,4 @@
-<template>
-  <SvgIcon name="facebook"></SvgIcon>
-</template>
+<template>About page</template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
-<script setup lang="ts">
-import SvgIcon from "@/components/SvgIcon/SvgIcon.vue";
-</script>
+<style></style>
+<script setup lang="ts"></script>
